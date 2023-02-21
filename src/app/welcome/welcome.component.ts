eb8f758a23d14c80f6fe32d4d9e4723a522af8de
kiranm889 <kiranm2636@gmail.com>
2023-02-21 15:04:00 +0530

    service-http-link

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { WelcomeDataService } from './../service/data/welcome-data.service';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent {
+  message = 'Some welcome msg';
+  name = '';
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
 
-  message="Some welcome msg"
-  name=''
-  constructor(private route:ActivatedRoute){
-
+  ngOnInit() {
+    console.log(this.message);
+    this.route.snapshot.params['name'];
+    this.name = this.route.snapshot.params['name'];
+    console.log(this.route.snapshot.params['name']);
   }
 
-  ngOnInit(){
-    console.log(this.message)
-    this.route.snapshot.params['name']
-    this.name=this.route.snapshot.params['name']
-    console.log(this.route.snapshot.params['name'])
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+    console.log('GET WELCOME MSG');
   }
-
 }
